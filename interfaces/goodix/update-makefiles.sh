ZVNexus/android_device_oneplus_oneplus9

op9: Import Goodix reverse engineered interface.
Change-Id: I49384962c417d93ccb1d708b8b219c73328561e9

interfaces/goodix/update-makefiles.sh
@@ -1,0 +1,8 @@
+#!/bin/bash
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.goodix.hardware:device/oneplus/oneplus9/interfaces/goodix" \
+  "android.hardware:hardware/interfaces" \
+  "android.hidl:system/libhidl/transport"

StatiXOS/android_device_oneplus_common

interfaces/goodix/update-makefiles.sh
@@ -1,0 +1,8 @@
+#!/bin/bash
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.goodix.hardware:device/oneplus/common/interfaces/goodix" \
+  "android.hardware:hardware/interfaces" \
+  "android.hidl:system/libhidl/transport"